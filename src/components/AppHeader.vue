552a08f3fc9b791008e0e104c0ad7f2c6607fd86
tal0311 <tal.amit0311@gmail.com>
2023-03-23 00:42:22 +0200

    1. fonts by import Roboto, 2. header basic styling

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,10 @@
 <template>
- <header class="app-header">
-  <i v-html="$getSvg('logo')"></i>
+ <header class="app-header full main-layout">
+  <section class="header-container">
+   <i class="icon logo" v-html="$getSvg('logo')"></i>
+   <input type="search" name="search" placeholder="Search">
+   <i class="icon" v-html="$getSvg('like')"></i>
+  </section>
  </header>
 </template>
 
