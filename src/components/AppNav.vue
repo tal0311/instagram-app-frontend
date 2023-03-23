96a9fa0f8b307ca3467a2ca53dded5e2774f8ae7
tal0311 <tal.amit0311@gmail.com>
2023-03-23 12:08:27 +0200

    nav bar styles grid and border

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,19 +1,21 @@
 <template>
- <nav class="app-nav grid">
-  <RouterLink to="/">
-   <i v-html="$getSvg('home')"></i>
-  </RouterLink>
-  <i v-html="$getSvg('search')"></i>
-  <RouterLink to="/explore">
-   <i v-html="$getSvg('explore')"></i>
-  </RouterLink>
-  <RouterLink to="/direct">
-   <i v-html="$getSvg('inbox')"></i>
-  </RouterLink>
-  <i v-html="$getSvg('new-post')"></i>
-  <RouterLink to="/username">
-   <i v-html="$getSvg('profile')"></i>
-  </RouterLink>
+ <nav class="app-nav full main-layout">
+  <div class="nav-container grid">
+   <RouterLink to="/">
+    <i v-html="$getSvg('home')"></i>
+   </RouterLink>
+   <i v-html="$getSvg('search')"></i>
+   <RouterLink to="/explore">
+    <i v-html="$getSvg('explore')"></i>
+   </RouterLink>
+   <RouterLink to="/direct">
+    <i v-html="$getSvg('inbox')"></i>
+   </RouterLink>
+   <i v-html="$getSvg('new-post')"></i>
+   <RouterLink to="/username">
+    <i v-html="$getSvg('profile')"></i>
+   </RouterLink>
+  </div>
  </nav>
 </template>
 
