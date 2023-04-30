c83f23915791ab9f8e8ef5259568a9936c40fe89
tal0311 <tal.amit0311@gmail.com>
2023-04-30 23:03:21 +0300

    sprite service

--- src/services/sprite.service.js
+++ src/services/sprite.service.js
@@ -6,23 +6,12 @@ export const spriteService = {
 function getSprite(type) {
 
  const spriteOpts = {
-  'logo-shadow': `background-position: -307px 0px;
-    width: 62px;
-    height: 63px;`,
+  'logo-shadow': 'background-position: -307px 0px; width: 62px; height: 63px;',
   'big-hart': 'background-position: -5px -22px; width: 113px; height: 95px;',
-  'lock': 'background-position: -128px 3px; width: 100px;  height: 100px; ',
-  'lock-blue': 'background-position: -227px 1px; width: 78px;   height: 78px; ',
-  'logo-withe': `background-position: -371px 0px;
-    width: 60px;
-    height: 60px;`,
-  'tag': 'tagged you in a post',
-  'save': 'saved your post',
-  'unsave': 'unsaved your post',
-  'share': 'shared your post',
-  'request': 'requested to follow you',
-  'accept': 'accepted your follow request',
-  'save': 'saved your post',
-  'send': 'sent you a message',
+  'lock': 'background-position: -128px 3px; width: 100px; height: 100px;',
+  'lock-blue': 'background-position: -227px 1px; width: 78px; height: 78px; ',
+  'logo-withe': 'background-position: -371px 0px; width: 60px; height: 60px;',
+
 
 
  }
