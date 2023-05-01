a43c93543b4d0f44fe5628a60b7bea80706adfef
tal0311 <tal.amit0311@gmail.com>
2023-05-01 23:20:59 +0300

    sprite service

--- src/services/sprite.service.js
+++ src/services/sprite.service.js
@@ -11,8 +11,12 @@ function getSprite(type) {
   'lock': 'background-position: -128px 3px; width: 100px; height: 100px;',
   'lock-blue': 'background-position: -227px 1px; width: 78px; height: 78px; ',
   'logo-withe': 'background-position: -371px 0px; width: 60px; height: 60px;',
-
-
+  'logo-display': 'background-position: 6px -126px; width: 182px; height: 57px;',
+  'story-arrow-left': 'background-position: -131px -100px; width: 28px; height: 26px;',
+  'story-arrow-left': 'background-position: -163px -100px; width: 28px; height: 26px;',
+  'tunnel': 'background-position: -193px -97px; width: 32px; height: 32px;',
+  'circle-play-btn': 'background-position: -227px -77px; width: 78px; height: 78px;',
+  'story-circle': 'background-position: -304px -64px; width: 68px; height: 68px;'
 
  }
 
